66358243dd39cfb01da9e1fa16a1134173a1986b
infern <semenmochalov55@gmail.com>
2023-08-22 01:07:50 +0700

    Вторая практика

--- script.js
+++ script.js
@@ -10,13 +10,37 @@ const personalMovieDB = {
     privat: false,
 }
 
-const firstMovieName = prompt("Один из последних просмотренных фильмов?"),
-    firstMovieRate = prompt("На сколько оцените его?"),
-    secondMovieName = prompt("Один из последних просмотренных фильмов?"),
-    secondMovieRate = prompt("На сколько оцените его?");
-
-
-personalMovieDB.movies[firstMovieName] = firstMovieRate;
-personalMovieDB.movies[secondMovieName] = secondMovieRate;
+for (let i = 0; i < personalMovieDB.count; i++) {
+    let movieName = prompt("Один из последних просмотренных фильмов?");
+    if (movieName == "" || movieName === null || movieName.length > 50) {
+        i--;
+        continue;
+    }
+    let movieRate = prompt("На сколько оцените его?");
+    if (movieRate == "" || movieRate === null) {
+        i--;
+        continue;
+    }
+    personalMovieDB.movies[movieName] = movieRate;
+}
 
 console.log(personalMovieDB);
+
+switch (true) {
+    case personalMovieDB.count < 10: {
+        alert("Просмотрено довольно мало фильмов");
+        break;
+    }
+    case personalMovieDB.count >= 10 || personalMovieDB.count <= 30: {
+        alert("Вы классический зритель");
+        break;
+    }
+    case personalMovieDB.count > 30: {
+        alert("Вы киноман");
+        break;
+    }
+    default: {
+        alert("Произошла ошибка")
+    }
+
+}
