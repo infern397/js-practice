9ad0a1fa7bac0cc647ba66693d86d606203b09f4
infern <semenmochalov55@gmail.com>
2023-08-22 20:06:43 +0700

    Четвертая практика

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
-let numberOfFilms;
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-}
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
-
-start();
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        let movieName = prompt("Один из последних просмотренных фильмов?");
-        if (movieName == "" || movieName === null || movieName.length > 50) {
-            i--;
-            continue;
-        }
-        let movieRate = prompt("На сколько оцените его?");
-        if (movieRate == "" || movieRate === null) {
-            i--;
-            continue;
-        }
-        personalMovieDB.movies[movieName] = movieRate;
-    }
-}
-
-rememberMyFilms();
-
-
-
-function detectPersonalLevel() {
-    switch (true) {
-        case personalMovieDB.count < 10: {
-            alert("Просмотрено довольно мало фильмов");
-            break;
-        }
-        case personalMovieDB.count >= 10 || personalMovieDB.count <= 30: {
-            alert("Вы классический зритель");
-            break;
-        }
-        case personalMovieDB.count > 30: {
-            alert("Вы киноман");
-            break;
-        }
-        default: {
-            alert("Произошла ошибка")
-        }
+    start: function() {
+        this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
     
+        while (this.count == '' || this.count == null || isNaN(this.count)) {
+            this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            let movieName = prompt("Один из последних просмотренных фильмов?");
+            if (movieName == "" || movieName === null || movieName.length > 50) {
+                i--;
+                continue;
+            }
+            let movieRate = prompt("На сколько оцените его?");
+            if (movieRate == "" || movieRate === null) {
+                i--;
+                continue;
+            }
+            this.movies[movieName] = movieRate;
+        }
+    },
+    detectPersonalLevel: function() {
+        switch (true) {
+            case this.count < 10: {
+                alert("Просмотрено довольно мало фильмов");
+                break;
+            }
+            case this.count >= 10 || this.count <= 30: {
+                alert("Вы классический зритель");
+                break;
+            }
+            case this.count > 30: {
+                alert("Вы киноман");
+                break;
+            }
+            default: {
+                alert("Произошла ошибка")
+            }
+        
+        }
+    },
+    showMyDB: function() {
+        if (!this.privat) {
+            console.log(this);
+        }
+    },
+    writeYourGenres: function() {
+        let genere;
+        for (let i = 0; i < 3; i++) {
+            genere = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+            if (genere == '' || genere == null) {
+                i--;
+                continue;
+            }
+            this.genres[i] = genere;
+        }
+        this.genres.forEach((gen, num) => console.log(
+            `Любимый жанр ${num + 1} - это ${gen}`
+        ))
+    },
+    toggleVisibleMyDB: function() {
+        this.privat = !this.privat;
     }
 }
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
 
-showMyDB(personalMovieDB.privat);
-
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-    }
-}
-
-writeYourGenres();
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel()
+personalMovieDB.showMyDB();
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB();
